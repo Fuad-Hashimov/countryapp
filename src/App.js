3ff25fd99135da2f9a6dc3f767ff14ff8b76df9a
FuadHashimov91 <fuadbh@code.edu.az>
2021-11-10 18:59:26 +0400

    Fetch Data from Api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
- 
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      const { data } = res;
+      const result = data.map((m) => {
+        return {
+          name: m.name.common,
+          official: m.name.official,
+          capital: m.capital,
+          region: m.region,
+          flags: m.flags.png,
+        };
+      });
+      setState(result);
+    });
+  }, []);
+  console.log(state);
+
   return (
     <div className="App">
       <header className="App-header">
